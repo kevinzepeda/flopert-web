d017e1b5858cfceacb83bbd28ee4374c7a9f1112
Kevin Zepeda <kevin@floups.com>
2021-07-12 13:41:56 -0500

    Change Icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [ 
     { src: "~/plugins/typed.js", ssr: true },
-    '~/plugins/gtm',
+    '~/plugins/gtm'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
